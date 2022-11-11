291dc05d4abbad2c644d0af352e185dde15e608b
Alejandro Salas <asalas.wd@outlook.com>
2022-11-11 13:59:54 -0500

    Generate constructor for RadioButtons class

--- src/com/java21days/RadioButtons.java
+++ src/com/java21days/RadioButtons.java
@@ -6,4 +6,11 @@ import java.awt.*;
 public class RadioButtons extends JFrame {
     Dimension dim = new Dimension(200, 200);
 
+    RadioButtons(String title){
+        super(title);
+        setSize(dim);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
